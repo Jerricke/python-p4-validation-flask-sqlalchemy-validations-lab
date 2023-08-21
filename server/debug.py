e9328b04eba2dd54ff30cf1e70db5d49c0397579
Jerricke <eejerrickw@gmail.com>
2023-08-21 12:47:23 -0600

    completo

--- server/debug.py
+++ server/debug.py
@@ -4,8 +4,7 @@ from app import app
 from models import db, Author, Post
 
 
-
-if __name__ == '__main__':
-    
+if __name__ == "__main__":
     with app.app_context():
-        import ipdb; ipdb.set_trace()
+        author = Author(name="", phone_number="1231144321")
+        # import ipdb; ipdb.set_trace()
